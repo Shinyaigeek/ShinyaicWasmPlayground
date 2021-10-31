8467db14e7306131ce0df44ca427a990ed5722e9
Shinyaigeek <Philispaxil@gmail.com>
2021-10-31 14:48:25 +0900

    Fix: app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,59 @@
 <script>
   export let url;
 
+  import init, * as wam from "./pkg/shinyaic_wasm";
+
   async function paint() {
-    const wasm = await import(
-      "/pkg/shinyaic_wasm.js"
-    );
+    const run = async () => {
+      await init("./pkg/shinyaic_wasm_bg.wasm");
+      window.wam = wam;
+    };
 
-	const mod = await wasm.default();
+	await run();
 
-	mod.greet()
+    const html = await (async () => {
+      if (url && url.length > 0) {
+        const res = await fetch(`/api/fetch.ts?target=${url}`);
+        const html = await res.text();
+        return html;
+      } else {
+        return `<!doctype html>
+    <html>
+    <head>
+    <style>
+    h1 {
+        font-size: 24px;
+        padding:4px;
+    }
+
+    body {
+        background: #ffffef;
+    }
+
+    #box {
+        background: #fff;
+        padding: 12px;
+        margin: 36px auto;
+    }
+
+    </style>
+    </head>
+    <body>
+        <div id="box">
+        <h1>Hello, Shinyaic Browser!</h1>
+        <p>
+            shinyaic is a browser made by @Shinyaigeek
+        </p>
+        </div>
+    </body>
+    </html>`;
+      }
+    })();
+
+    console.log(html);
+	console.log(wam)
+
+    wam.browser(html);
   }
 </script>
 
